e72f673f4a8baaf2262e8b005a88c6cd9b66ce84
Baby_cake <solrunasta@hotmail.com>
2019-12-10 23:29:45 +0000

    fixes for main

--- src/views/Main/index.jsx
+++ src/views/Main/index.jsx
@@ -7,21 +7,23 @@ import * as Animatable from 'react-native-animatable';
 import styles from './style';
 import logo from '../../resources/LOGO.png';
 
-const Main = () => {
-  return (
-    <View style={styles.container}>
-      <Image animation="slideInDown" style={styles.logo} source={logo} />
-      <Animatable.Text animation="slideInDown" iterationCount={1} direction="alternative" style={styles.logoText}>Think big!</Animatable.Text>
-      <View style={{padding: 20}}>
-        <TouchableHighlight style={styles.button} onPress={()=>this.props.navigation.navigate('Cinemas')}>
-          <Text style={styles.buttonText}>Cinemas</Text>
-        </TouchableHighlight>
-        <TouchableHighlight style={styles.button} onPress={() =>this.props.navigation.navigate('UpcomingMovies')}>
-          <Text style={styles.buttonText}>Upcoming Movies</Text>
-        </TouchableHighlight>
+class Main extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image animation="slideInDown" style={styles.logo} source={logo} />
+        <Animatable.Text animation="slideInDown" iterationCount={1} direction="alternative" style={styles.logoText}>Think big!</Animatable.Text>
+        <View style={{padding: 20}}>
+          <TouchableHighlight style={styles.button} onPress={()=>this.props.navigation.navigate('Cinemas')}>
+            <Text style={styles.buttonText}>Cinemas</Text>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.button} onPress={() =>this.props.navigation.navigate('UpcomingMovies')}>
+            <Text style={styles.buttonText}>Upcoming Movies</Text>
+          </TouchableHighlight>
+        </View>
       </View>
-    </View>
-  );
+    );
+  }
 }
 
 
