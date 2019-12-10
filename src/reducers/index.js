81325f453d7fe419b6f7b52bfc96c65ada0f6e1b
Baby_cake <solrunasta@hotmail.com>
2019-12-10 22:33:58 +0000

    working to on movie from constants to view

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import cinemas from './CinemasReducer';
+import movie from './MovieReducer';
 
 export default combineReducers({
   cinemas,
+  movie,
 });
--- src/views/Main/index.jsx
+++ src/views/Main/index.jsx
@@ -7,23 +7,21 @@ import * as Animatable from 'react-native-animatable';
 import styles from './style';
 import logo from '../../resources/LOGO.png';
 
-class Main extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Image animation="slideInDown" style={styles.logo} source={logo} />
-        <Animatable.Text animation="slideInDown" iterationCount={1} direction="alternative" style={styles.logoText}>Think big!</Animatable.Text>
-        <View style={{padding: 20}}>
-          <TouchableHighlight style={styles.button} onPress={()=>this.props.navigation.navigate('Cinemas')}>
-            <Text style={styles.buttonText}>Cinemas</Text>
-          </TouchableHighlight>
-          <TouchableHighlight style={styles.button} onPress={() =>this.props.navigation.navigate('UpcomingMovies')}>
-            <Text style={styles.buttonText}>Upcoming Movies</Text>
-          </TouchableHighlight>
-        </View>
+const Main = () => {
+  return (
+    <View style={styles.container}>
+      <Image animation="slideInDown" style={styles.logo} source={logo} />
+      <Animatable.Text animation="slideInDown" iterationCount={1} direction="alternative" style={styles.logoText}>Think big!</Animatable.Text>
+      <View style={{padding: 20}}>
+        <TouchableHighlight style={styles.button} onPress={()=>this.props.navigation.navigate('Cinemas')}>
+          <Text style={styles.buttonText}>Cinemas</Text>
+        </TouchableHighlight>
+        <TouchableHighlight style={styles.button} onPress={() =>this.props.navigation.navigate('UpcomingMovies')}>
+          <Text style={styles.buttonText}>Upcoming Movies</Text>
+        </TouchableHighlight>
       </View>
-    );
-  }
+    </View>
+  );
 }
 
 
