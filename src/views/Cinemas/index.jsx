ea0cf4f8b67c4cedaf7cfd27ea9f7dd4406ecb56
nannalop <natalia17@ru.is>
2019-12-10 18:59:39 +0000

    laga drasl

--- src/views/Cinemas/index.jsx
+++ src/views/Cinemas/index.jsx
@@ -1,22 +1,9 @@
 import React from 'react';
-<<<<<<< HEAD
-import { View } from 'react-native';
 import { CinemaDetail } from '../CinemaDetail';
-=======
 import { View, Text, Image} from 'react-native';
->>>>>>> 2dc323e3f28eb8278b00823d48a3178a4d2f0213
 import styles from './style';
 import * as Animatable from 'react-native-animatable';
-import text_logo from '../../resources/Thinkbig.png'
 
-<<<<<<< HEAD
-const Cinemas = ({ children }) => (
-  <View style={styles.container}>
-    <CinemaDetail />
-    { children }
-  </View>
-);
-=======
 class Cinemas extends React.Component {
   render() {
     return (
@@ -26,6 +13,4 @@ class Cinemas extends React.Component {
     )
   }
 }
->>>>>>> 2dc323e3f28eb8278b00823d48a3178a4d2f0213
-
 export default Cinemas;
